18b5be4a69933d13a668d97547e6258d6cac8460
Fred Duguay <fredduguay@users.noreply.github.com>
2022-04-26 17:39:36 -0400

    String Methods

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Vanilla JS query selector
 document.querySelector('h1').style.color = 'red';
 
+// ################
 //#CONSOLE
+// ################
 console.table({ a: 1, b: 2 });
 
 console.error('This is an error');
@@ -19,7 +21,9 @@ console.log('Hello World');
 console.log('Hello World');
 console.timeEnd('Hello');
 
+// ################
 //#VARIABLES AND DATA TYPES
+// ################
 const person = {
   name: 'John',
   age: 30
@@ -40,7 +44,9 @@ console.log(person, numbers);
 const sym = Symbol();
 console.log(typeof sym);
 
+// ################
 //#TYPE CONVERSION
+// ################
 val1 = String(4 + 4);
 val2 = [1, 2, 3, 4].toString();
 val3 = Number('5');
@@ -58,7 +64,9 @@ console.log(val2.length);
 // val3 with 2 decimals
 console.log(val3.toFixed(2));
 
-// #The Math Object
+// ################
+// #THE MATH OBJECT
+// ################
 let val;
 val = Math.PI;
 val = Math.E;
@@ -81,3 +89,51 @@ val = Math.min(2, 33, 1, 444, 55, 35);
 val = Math.random();
 //Random number between 1 and 20
 val = Math.floor(Math.random() * 20 + 1);
+
+// #STRING METHODS - CONCATENATION AND TEMPLATE LITERALS
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 34;
+const str = 'Hello there my name is Brad';
+const tags = 'Web Development, Programming, Design';
+
+//concat()
+val = firstName.concat(' ', lastName);
+
+//toUpperCase(), toLowerCase()
+val = firstName.toLowerCase();
+val = firstName.toUpperCase();
+
+//indexOf() - 2 (William)
+val = firstName.indexOf('l');
+
+//indexOf() - 3 (William)
+val = firstName.lastIndexOf('l');
+
+//Escaping
+// val = 'That\'s awesome, I can\'t wait'
+
+//l charAt()
+val = firstName.charAt('2');
+
+//Get last char
+val = firstName.charAt(firstName.length - 1);
+
+//substring() - Will
+val = firstName.substring(0, 4);
+
+//slice() - Will
+val = firstName.slice(0, 4); // first 4
+
+//slice() - iam
+val = firstName.slice(-3); // last 3
+
+//split()
+str.split(' ');
+tags.split(',');
+
+//replace()
+str.replace('Brad', 'Jack');
+
+//includes
+val = str.includes('foo');
