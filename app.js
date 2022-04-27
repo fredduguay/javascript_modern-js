6906142cdad5518958a28e68bfc3a63ee6b834c2
Fred Duguay <fredduguay@users.noreply.github.com>
2022-04-27 17:06:46 -0400

    Window Methods / Object / Properties

--- app.js
+++ app.js
@@ -431,3 +431,71 @@ const user = {
 for (let x in user) {
   console.log(`${x} : ${user[x]}`);
 }
+
+// ################
+// #WINDOW METHODS/ OBJECT / PROPERTIES
+// ################
+
+window.console.log(123);
+
+window.alert('Hello World');
+
+//Prompt
+const input = prompt();
+alert(input);
+
+//Confirm
+if (confirm('Are you sure')) {
+  console.log('Yes');
+} else {
+  console.log('NO');
+}
+
+let val;
+
+//Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+//Inner height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+//Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+//Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+
+//Returns protocol, hostname and port
+val = window.location.href;
+
+//Search (Returns query string)
+val = window.location.search;
+
+//Redirect
+window.location.href = 'http://google.com';
+
+//Reload
+window.location.reload();
+
+//History Object
+window.history.go(-2);
+val = window.history.length;
+
+//Navigator Object
+val = window.navigator;
+`
+`;
+val = window.navigator.vendor;
+val = window.navigator.userAgent;
+val = window.navigator.language;
+
+//Deprecated
+val = window.navigator.platform;
+val = window.navigator.appName;
+
+console.log(val);
