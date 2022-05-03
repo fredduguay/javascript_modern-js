38a818c28663133fbb9b6f74de5ab8271441651e
Fred Duguay <fredduguay@users.noreply.github.com>
2022-05-03 10:17:13 -0400

    Event bubbling and delegatrion

--- dom.js
+++ dom.js
@@ -395,3 +395,41 @@ function fireEvent(e) {
 
   e.preventDefault();
 }
+
+// ################
+// #EVENT BUBBLING AND DELEGATION
+// ################
+
+//Event bubbling
+document.querySelector('.card-title').addEventListener('click', function () {
+  console.log('card title');
+});
+
+document.querySelector('.card-content').addEventListener('click', function () {
+  console.log('card content');
+});
+
+document.querySelector('.card').addEventListener('click', function () {
+  console.log('card');
+});
+
+document.querySelector('.col').addEventListener('click', function () {
+  console.log('col');
+});
+
+//Event delegation
+// const delItem = document.querySelector('.delete-item');
+// deleteItem.addEventListener('click', deleteItem);
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+  // if(e.target.className === 'fa fa-remove') {
+  // }
+
+  // target delete button in li item
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    console.log('delete item');
+    //delete li item (parent of clicked button)
+    e.target.parentElement.parentElement.remove();
+  }
+}
