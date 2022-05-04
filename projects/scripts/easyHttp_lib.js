407861ca1083d2ffa3786e87b4765f830879b7f9
Fred Duguay <fredduguay@users.noreply.github.com>
2022-05-04 10:22:29 -0400

    Custom Http library with ajax and callbacks

--- projects/scripts/easyHttp_lib.js
+++ projects/scripts/easyHttp_lib.js
@@ -11,7 +11,7 @@ easyHTTP.prototype.get = function (url, callback) {
     if (self.http.status === 200) {
       //action can be passed as callback
 
-      //callback(error paramenter, response)
+      //callback(error parameter, response)
       callback(null, self.http.responseText);
     } else {
       callback('Error: ' + self.http.status);
@@ -22,5 +22,46 @@ easyHTTP.prototype.get = function (url, callback) {
 };
 
 //Make and HTTP POST Request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 //Make and HTTP PUT Request
+
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 //Make and HTTP DELETE Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      //action can be passed as callback
+
+      //callback(error parameter, response)
+      callback(null, 'Post deleted');
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
